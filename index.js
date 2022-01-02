735ed9bf43eca2b134a6edaa0011d78166c52a10
aviation4 <57131223+aviation4@users.noreply.github.com>
2022-01-02 18:26:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,147 +1,18 @@
-/* Declaring variables */
-var inputBill = document.getElementById("input-bill");
-var inputPeople = document.getElementById("input-people");
-var inputArray = document.querySelectorAll(".input");
-var buttonTipArray = document.querySelectorAll(".button--tip-percentage");
-var isTipButtonEnabled = 0;
-var resetButton = document.getElementById("button__reset");
+/* Declaring letiables */
+let inputBill = document.getElementById("input-bill");
+let inputPeople = document.getElementById("input-people");
+let inputArray = document.querySelectorAll(".input");
+let buttonTipArray = document.querySelectorAll(".button--tip-percentage");
+let isTipButtonEnabled = 0;
+let resetButton = document.getElementById("button__reset");
 
-var resultTip = document.getElementById("result-tip");
-var resultTotal = document.getElementById("result-total");
-var wasInputModified = [0, 0];
+let resultTip = document.getElementById("result-tip");
+let resultTotal = document.getElementById("result-total");
+let wasInputModified = [0, 0];
 
-var tipFactor;
+let tipFactor;
 
-var breakpoint = window.matchMedia("(min-width: 1400px)");
-
-
-
-/*** When tip button is clicked
-     (change tip button states //
-     enable reset button //
-     calculate results) ***/
-
-/* For every tip button */
-for (let i = 0; i < buttonTipArray.length; i++) {
-
-  /* Assign Event Listener */
-  buttonTipArray[i].addEventListener("click", function() {
-
-    /* Check if clicked tip button was already enabled */
-    var alreadyIsEnabled = 0;
-    if (this.classList.contains("button--tip-percentage--enabled")) {
-      alreadyIsEnabled = 1;
-    }
-
-
-    /* Disable all enabled tip buttons */
-    for (let i = 0; i < buttonTipArray.length; i++) {
-      if (buttonTipArray[i].classList.contains("button--tip-percentage--enabled")) {
-        buttonTipArray[i].classList.remove("button--tip-percentage--enabled");
-        isTipButtonEnabled = 0;
-        break;
-      }
-    }
-
-
-    /* Enable (if was already disabled) */
-    if (alreadyIsEnabled == 0) {
-      this.classList.add("button--tip-percentage--enabled");
-      isTipButtonEnabled = 1;
-    }
-
-
-    /* Enable reset button */
-    resetButton.classList.add("button--reset--enabled");
-
-
-    /* Calculate when tip button toggle */
-    /* If inputs are modified and tip button is enabled */
-    if (wasInputModified.every(el => el == 1) && isTipButtonEnabled == 1) {
-
-
-      /* Calculate tip factor based on enabled tip button */
-      for (let i = 0; i < buttonTipArray.length; i++) {
-        if (buttonTipArray[i].classList.contains("button--tip-percentage--enabled")) {
-          tipFactor = 1 + (buttonTipArray[i].value) / 100;
-          break;
-        }
-      }
-
-      /* Calculate results */
-      var resultTipNotRounded = ((inputBill.value * tipFactor) - inputBill.value) / inputPeople.value;
-      resultTip.innerHTML = "$" + Math.round(resultTipNotRounded * 100) / 100;
-      resultTotal.innerHTML = "$" + Math.round(inputBill.value * tipFactor / inputPeople.value * 100) / 100;
-
-
-
-    } else if (wasInputModified.every(el => el == 0) && isTipButtonEnabled == 0) {
-      /* Disable reset button if all inputs inactive */
-      resetButton.classList.remove("button--reset--enabled");
-
-    } else if (wasInputModified.some(el => el == 0) || isTipButtonEnabled == 0) {
-      /* Set results to $0, if any input is incomplete */
-      resultTip.innerHTML = "$" + "0";
-      resultTotal.innerHTML = "$" + "0";
-
-    }
-
-
-  });
-}
-
-
-
-
-/**** Calculate when input modifies ****/
-for (let i = 0; i < inputArray.length; i++) {
-
-  inputArray[i].addEventListener("input", function(event) {
-
-    wasInputModified[i] = 1;
-    /* Enable reset buton */
-    resetButton.classList.add("button--reset--enabled");
-
-    var insertedSign = event.data;
-    var hyphen = "-";
-    var previousSigns = inputArray[i].value;
-    /*
-    console.log(insertedSign);
-    console.log(event.key);
-    console.log(event.data);*/
-
-    if (insertedSign == hyphen || previousSigns.includes(hyphen)) {
-
-
-
-    } else {
-
-
-
-      /* If inputs are modified and tip button is enabled */
-      if (wasInputModified.every(el => el == 1) && isTipButtonEnabled == 1) {
-
-        /* Calculate tip factor based on enabled tip button */
-        for (let i = 0; i < buttonTipArray.length; i++) {
-          if (buttonTipArray[i].classList.contains("button--tip-percentage--enabled")) {
-            tipFactor = 1 + (buttonTipArray[i].value) / 100;
-          }
-        }
-
-        /* Calculate results */
-        var resultTipNotRounded = ((inputBill.value * tipFactor) - inputBill.value) / inputPeople.value;
-        resultTip.innerHTML = "$" + Math.round(resultTipNotRounded * 100) / 100;
-        resultTotal.innerHTML = "$" + Math.round(inputBill.value * tipFactor / inputPeople.value * 100) / 100;
-
-      }
-
-
-    }
-
-
-  });
-
-}
+let breakpoint = window.matchMedia("(min-width: 1400px)");
 
 
 if (breakpoint.matches) {
@@ -153,93 +24,232 @@ if (breakpoint.matches) {
 }
 
 
-inputArray[0].addEventListener("keydown", function() {
-  var regex = /[a-z]/i;
 
-  alert(event.key);
-  alert(event.code);
-  alert(event.keyCode);
-  alert(inputArray[0].value);
 
-  if (event.key == ",") {
-    document.getElementById("warning-info-bill").style.opacity = "1";
-    document.getElementById("warning-info-bill").innerHTML = "Use period (.) operator";
-    event.preventDefault();
-  } else if (event.key == "-") {
-    document.getElementById("warning-info-bill").style.opacity = "1";
-    document.getElementById("warning-info-bill").innerHTML = "Must be greater than zero";
-    event.preventDefault();
-  } else if (regex.test(event.key) &&
-      event.key !== "Backspace" &&
-      event.key !== "Delete" &&
-      event.key !== "ArrowLeft" &&
-      event.key !== "ArrowRight" ) {
-    document.getElementById("warning-info-bill").style.opacity = "1";
-    document.getElementById("warning-info-bill").innerHTML = "Must be a number";
-    event.preventDefault();
-  }
-  else {
-    document.getElementById("warning-info-bill").style.opacity = "0";
-  }
+/***** For every tip button *****/
+buttonTipArray.forEach(function(el, i) {
 
+  /***** Assign Event Listener *****/
+  el.addEventListener("click", function() {
+
+
+    /* Enable or disable tip buttons */
+    tipButtonToggler(el);
+
+
+    /* Enable reset button */
+    enableResetButton();
+
+
+    /* Update results (calculate or reset) */
+    updateResults(el);
+
+
+  });
 
 });
 
 
 
+/***** For both inputs *****/
+inputArray.forEach(function(el, i) {
 
 
-inputArray[0].addEventListener("blur", function () {
-      document.getElementById("warning-info-bill").style.opacity = "0";
-})
-
-inputArray[1].addEventListener("blur", function () {
-      document.getElementById("warning-info-people").style.opacity = "0";
-})
-
+  /**** Key-by-key validation ****/
+  el.addEventListener("keydown", function() {
+    keydownValidation(el, i);
+  });
 
 
 
-inputArray[1].addEventListener("keydown", function() {
-  var regex = /[a-z]/i;
-  alert(event.key);
-  alert(event.code);
-  alert(event.keyCode);
-  alert(inputArray[0].value);
-  if (event.key == "," || event.key == ".") {
-    document.getElementById("warning-info-people").style.opacity = "1";
-    document.getElementById("warning-info-people").innerHTML = "Must be an integer";
-    event.preventDefault();
-  } else if (event.key == "-") {
-    document.getElementById("warning-info-people").style.opacity = "1";
-    document.getElementById("warning-info-people").innerHTML = "Must be greater than zero";
-    event.preventDefault();
-  } else if (regex.test(event.key) &&
-      event.key !== "Backspace" &&
-      event.key !== "Delete" &&
-      event.key !== "ArrowLeft" &&
-      event.key !== "ArrowRight" ) {
-    document.getElementById("warning-info-people").style.opacity = "1";
-    document.getElementById("warning-info-people").innerHTML = "Must be a number";
-    event.preventDefault();
-  } else {
-    document.getElementById("warning-info-people").style.opacity = "0";
-  }
+  /**** Post-focus validation ****/
+  el.addEventListener("blur", function() {
+    blurValidation(el, i);
+  });
+
+
+
+  /**** Calculate when input modifies ****/
+  el.addEventListener("input", function(event) {
+
+
+    /* Enable reset buton */
+    enableResetButton();
+
+
+    /* Update results (calculate or reset) */
+    updateResults(el);
+
+
+  });
+
 });
-
-
-
 
 
 
 /*** Resetting ***/
 resetButton.addEventListener("click", function() {
 
+  resetAll();
+
+})
+
+
+function tipButtonToggler(el) {
+
+  if (el.classList.contains("button--tip-percentage--enabled")) {
+    el.classList.remove("button--tip-percentage--enabled");
+    isTipButtonEnabled = 0;
+  } else if (isTipButtonEnabled == 1) {
+    buttonTipArray.forEach(el => el.classList.remove("button--tip-percentage--enabled"));
+    el.classList.add("button--tip-percentage--enabled");
+  } else {
+    el.classList.add("button--tip-percentage--enabled");
+    isTipButtonEnabled = 1;
+  }
+
+}
+
+
+function keydownValidation (el, i) {
+
+  let regex = /[a-z]/i;
+  let currentInput;
+
+  if (el == inputArray[0]) {
+    currentInput = document.getElementById("warning-info-bill");
+  } else if (el == inputArray[1]) {
+    currentInput = document.getElementById("warning-info-people");
+  }
+
+  currentInput.style.opacity = "1";
+  wasInputModified[i] = 0;
+
+  if (event.key == "," && i == 0) {
+    currentInput.innerHTML = "Use period (.) operator";
+
+  } else if ((event.key == "," || event.key == ".") && i == 1) {
+    currentInput.innerHTML = "Must be an integer";
+
+  } else if (event.key == "-") {
+    currentInput.innerHTML = "Must be greater than zero";
+
+  } else if (regex.test(event.key) &&
+    event.key !== "Backspace" &&
+    event.key !== "Delete" &&
+    event.key !== "ArrowLeft" &&
+    event.key !== "ArrowRight") {
+    currentInput.innerHTML = "Must be a number";
+
+  } else {
+    currentInput.style.opacity = "0";
+    currentInput.innerHTML = "";
+    wasInputModified[i] = 1;
+  }
+
+}
+
+
+function blurValidation (el, i) {
+
+  let currentInput;
+
+  if (i == 0) {
+    currentInput = document.getElementById("warning-info-bill");
+  } else if (i == 1) {
+    currentInput = document.getElementById("warning-info-people");
+  }
+
+  currentInput.style.opacity = "1";
+  console.log(Number(el.value));
+  console.log(el.value)
+
+  if (el.value.includes("-") == 0 && el.value == "") {
+    currentInput.innerHTML = "Must be a number";
+  } else if (el.value.includes(".") && i == 1) {
+    currentInput.innerHTML = "Must be an integer";
+  } else if (el.value < 0) {
+    currentInput.innerHTML = "Must be greater than zero";
+  } else {
+    currentInput.style.opacity = "0";
+    currentInput.innerHTML = "";
+  }
+
+}
+
+
+function calculate(el) {
+
+  if (wasInputModified.every(el => el == 1) && isTipButtonEnabled == 1) {
+
+    calculateTipFactor();
+    calculateResults();
+
+  } else if (wasInputModified.every(el => el == 0) && isTipButtonEnabled == 0) {
+
+    /* Disable reset button if all inputs inactive */
+    resetButton.classList.remove("button--reset--enabled");
+
+  } else if (wasInputModified.some(el => el == 0) || isTipButtonEnabled == 0) {
+
+    /* Set results to $0, if any input is incomplete */
+    resetResults();
+
+  }
+
+}
+
+
+function calculateTipFactor() {
+
+  /* Calculate tip factor based on enabled tip button */
+  buttonTipArray.forEach(function(el){
+    if (el.classList.contains("button--tip-percentage--enabled")) {
+      tipFactor = 1 + (el.value) / 100;
+    }
+  });
+
+}
+
+
+function calculateResults() {
+
+  let resultTipNotRounded = ((inputBill.value * tipFactor) - inputBill.value) / inputPeople.value;
+  resultTip.innerHTML = "$" + Math.round(resultTipNotRounded * 100) / 100;
+  resultTotal.innerHTML = "$" + Math.round(inputBill.value * tipFactor / inputPeople.value * 100) / 100;
+
+  if (resultTip.innerHTML == "$Infinity" || resultTotal.innerHTML == "$Infinity") {
+    resultTip.innerHTML = "$0";
+    resultTotal.innerHTML = "$0";
+  }
+
+}
+
+
+function enableResetButton() {
+
+  resetButton.classList.add("button--reset--enabled");
+
+}
+
+
+function resetResults() {
+
+  resultTip.innerHTML = "$" + "0";
+  resultTotal.innerHTML = "$" + "0";
+
+}
+
+
+function resetAll() {
+
   /* Reset input and result values */
   document.getElementById("input-bill").value = "";
   document.getElementById("input-people").value = "";
   document.getElementById("result-tip").innerHTML = "$" + 0;
   document.getElementById("result-total").innerHTML = "$" + 0;
+  document.getElementById("warning-info-bill").innerHTML = "";
+  document.getElementById("warning-info-people").innerHTML = "";
 
   /* Disable tip button */
   for (let i = 0; i < buttonTipArray.length; i++) {
@@ -251,7 +261,8 @@ resetButton.addEventListener("click", function() {
   /* Disable reset button */
   resetButton.classList.remove("button--reset--enabled");
 
-  /* Reset variables */
+  /* Reset letiables */
   isTipButtonEnabled = 0;
   wasInputModified = [0, 0];
-})
+
+}
