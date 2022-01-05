342796e7d51cde1799437c64fc7a4805ab815d50
aviation4 <57131223+aviation4@users.noreply.github.com>
2022-01-05 18:36:39 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -53,6 +53,26 @@ buttonTipEnabled = "button--tip-percentage--enabled";
 buttonResetEnabled = "button--reset--enabled";
 
 
+function checkTouchScreen (){
+
+
+  if ("maxTouchPoints" in navigator) {
+      hasTouchScreen = navigator.maxTouchPoints > 0;
+  } else if ("msMaxTouchPoints" in navigator) {
+      hasTouchScreen = navigator.msMaxTouchPoints > 0;
+  } else {
+      var mQ = window.matchMedia && matchMedia("(pointer:coarse)");
+      if (mQ && mQ.media === "(pointer:coarse)") {
+          hasTouchScreen = !!mQ.matches;
+      } else if ('orientation' in window) {
+          hasTouchScreen = true;
+      }
+  }
+
+  return hasTouchScreen;
+
+}
+
 checkTouchScreen ();
 
 
